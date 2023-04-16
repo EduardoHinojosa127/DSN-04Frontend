acb41cc685926f3ef534d062944d06ff8d3cd6bc
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2023-04-15 19:51:22 -0500

    DSN-Frontend

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const cors = require('cors');
+
+app.use(cors());
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pages/index.html');
+  res.render('index.ejs');
 });
 
 app.get('/clientes', (req, res) => {
-  res.sendFile(__dirname + '/pages/clientes.html');
+  axios.get('http://api:3000/clientes')
+    .then(response => {
+      const clientes = response.data;
+      res.render('clientes.ejs', { clientes });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Error al obtener los clientes');
+    });
 });
 
 app.get('/productos', (req, res) => {
-  res.sendFile(__dirname + '/pages/productos.html');
+  axios.get('http://api:3000/productos')
+    .then(response => {
+      const productos = response.data;
+      res.render('productos.ejs', { productos });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Error al obtener los productos');
+    });
 });
 
-app.listen(3000, () => {
-  console.log('La aplicación está corriendo en http://localhost:3000');
+app.listen(3001, () => {
+  console.log('La aplicación está corriendo en http://localhost:3001');
 });
